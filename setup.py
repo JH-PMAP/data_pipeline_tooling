2ee3eb7e6005a2d555d6dd276199cb124202b50a
Eric Schles <eschles3@jh.edu>
2022-08-15 12:43:17 -0400

    adding license

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="databricks_tooling",
-    version="0.01",
+    version="0.02",
     description="A library for databricks jobs api",
     long_description=README,
     author="Eric Schles",
