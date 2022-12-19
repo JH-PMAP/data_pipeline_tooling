6b364f36457c3e5d707f0a99832b06190bbc36e6
Eric Schles <eschles3@jh.edu>
2022-12-19 06:24:58 -0500

    adding changes /version bump

--- remove_eggs.sh
+++ remove_eggs.sh
@@ -3,5 +3,5 @@ rm -Rf build
 rm -Rf dist
 python -m pip uninstall -y data_pipeline_tooling
 
-rm -Rf /Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages/data_pipeline_tooling-0.9-py3.9.egg
-rm -Rf /Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/data_pipeline_tooling-0.9-py3.10.egg
+rm -Rf /Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages/data_pipeline_tooling-0.11-py3.9.egg
+rm -Rf /Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/data_pipeline_tooling-0.11-py3.10.egg
