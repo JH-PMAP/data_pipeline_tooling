b20018d96e51759bf18f727a0995cef129703da1
Lior Shahverdi <liorshahverdi@gmail.com>
2024-06-12 00:46:09 -0400

    NON-UC environment code file path in tooling.upload_task.

--- data_pipeline_tooling/tooling.py
+++ data_pipeline_tooling/tooling.py
@@ -319,8 +319,8 @@ class Orca:
         datalake_client.upload_file(
             file_path + file_name, directory + file_name, mode=mode
         )
-        return f"dbfs:/Volumes/project_k/storage/code/{directory}{file_name}"
-        #return f"dbfs:/mnt/pmpkdevdatalakefs/precgs_pipeline/{run_type}/{version_id}/{file_name}"
+        #return f"dbfs:/Volumes/project_k/storage/code/{directory}{file_name}"
+        return f"dbfs:/mnt/pmpkdevdatalakefs/precgs_pipeline/{run_type}/{version_id}/{file_name}"
 
     def execute_job(self, job_id):
         """
